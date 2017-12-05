2838450be69ec103906717b7c370ce179364905a
appsmehta <apoorv.mehta@sjsu.edu>
2017-12-05 05:01:39 -0800

    Checkout flow complete

--- app/src/main/java/com/librarymanagement/siddharth/snaplibrary/SuccessFragment.java
+++ app/src/main/java/com/librarymanagement/siddharth/snaplibrary/SuccessFragment.java
@@ -3,11 +3,17 @@ package com.librarymanagement.siddharth.snaplibrary;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.librarymanagement.siddharth.snaplibrary.helper.LogHelper;
+
+import java.util.ArrayList;
 
 
 public class SuccessFragment extends Fragment {
@@ -19,12 +25,22 @@ public class SuccessFragment extends Fragment {
     private TextView[] dueDates= new TextView[3];
     private TextView[] checkoutDates= new TextView[3];
 
+    private CardView c1;
+    private CardView c2;
+    private CardView c3;
+    ArrayList<PatronBookItem> checkedoutbooks = new ArrayList<PatronBookItem>();
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_success, container, false);
+
+        c1= (CardView)view.findViewById(R.id.success_fragment_cardview1);
+        c2= (CardView)view.findViewById(R.id.success_fragment_cardview2);
+        c3= (CardView)view.findViewById(R.id.success_fragment_cardview3);
 
         images[0]=(ImageView)view.findViewById(R.id.success_fragment_book_image1);
         images[1]=(ImageView)view.findViewById(R.id.success_fragment_book_image2);
@@ -45,8 +61,72 @@ public class SuccessFragment extends Fragment {
         checkoutDates[0]=(TextView)view.findViewById(R.id.success_fragment_checkout_date1);
         checkoutDates[1]=(TextView)view.findViewById(R.id.success_fragment_checkout_date2);
         checkoutDates[2]=(TextView)view.findViewById(R.id.success_fragment_checkout_date3);
-        getActivity().setTitle("Checkout Details");
+
+
+
+
+        Bundle responseBundle = getArguments();
+         checkedoutbooks =  (ArrayList<PatronBookItem>)responseBundle.getSerializable("checkoutbooksserial");
+        LogHelper.logMessage("Apoorv","Received books in success fragment:"+checkedoutbooks.size());
+
+        refreshView();
+
         return view;
+    }
+
+    void refreshView() {
+        switch (checkedoutbooks.size()){
+
+            case 0:
+                c1.setVisibility(View.GONE);
+                c2.setVisibility(View.GONE);
+                c3.setVisibility(View.GONE);
+
+                break;
+            case 1:
+                c2.setVisibility(View.GONE);
+                c3.setVisibility(View.GONE);
+                //TODO: Populate 1st Card
+                populateCard(1);
+
+                break;
+            case 2:
+                c3.setVisibility(View.GONE);
+                //TODO: Populate first two cards
+                populateCard(1);
+                populateCard(2);
+                break;
+            case 3:
+                //TODO: Populate all three cards
+                populateCard(1);
+                populateCard(2);
+                populateCard(3);
+                break;
+
+        }
+    }
+
+    void populateCard(int cardNumber) {
+        switch (cardNumber) {
+            case 1:
+                titles[0].setText(checkedoutbooks.get(0).Book_Title);
+                authors[0].setText(checkedoutbooks.get(0).Book_Author);
+                dueDates[0].setText("Due Date: "+checkedoutbooks.get(0).bookDueDate);
+                checkoutDates[0].setText("Checkout Date: "+checkedoutbooks.get(0).bookCheckedOutDate);
+                break;
+            case 2:
+                titles[1].setText(checkedoutbooks.get(1).Book_Title);
+                authors[1].setText(checkedoutbooks.get(1).Book_Author);
+                dueDates[1].setText("Due Date: "+checkedoutbooks.get(1).bookDueDate);
+                checkoutDates[1].setText("Checkout Date: "+checkedoutbooks.get(1).bookCheckedOutDate);
+                break;
+            case 3:
+                titles[2].setText(checkedoutbooks.get(2).Book_Title);
+                authors[2].setText(checkedoutbooks.get(2).Book_Author);
+                dueDates[2].setText("Due Date: "+checkedoutbooks.get(2).bookDueDate);
+                checkoutDates[2].setText("Checkout Date: "+checkedoutbooks.get(2).bookCheckedOutDate);
+                break;
+        }
     }
 
 }
