d8bdc750a0f68498e62e504de149ea701ab1c217
NEEL PATEL <neelpatel3284@gmail.com>
2017-12-04 18:34:41 -0800

    Listener is added for return button in return Fragment.

--- app/src/main/java/com/librarymanagement/siddharth/snaplibrary/ReturnFragment.java
+++ app/src/main/java/com/librarymanagement/siddharth/snaplibrary/ReturnFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 
 public class ReturnFragment extends Fragment {
 
+    private Button return_btn;
     private CheckBox cb1;
     private CheckBox cb2;
     private CheckBox cb3;
@@ -94,6 +96,43 @@ public class ReturnFragment extends Fragment {
         img8 = (ImageView)view.findViewById(R.id.return_fragment_book_image8);
         img9 = (ImageView)view.findViewById(R.id.return_fragment_book_image9);
 
+        Author1 = (TextView)view.findViewById(R.id.return_fragment_author1);
+        Author2 = (TextView)view.findViewById(R.id.return_fragment_author2);
+        Author3 = (TextView)view.findViewById(R.id.return_fragment_author3);
+        Author4 = (TextView)view.findViewById(R.id.return_fragment_author4);
+        Author5 = (TextView)view.findViewById(R.id.return_fragment_author5);
+        Author6 = (TextView)view.findViewById(R.id.return_fragment_author6);
+        Author7 = (TextView)view.findViewById(R.id.return_fragment_author7);
+        Author8 = (TextView)view.findViewById(R.id.return_fragment_author8);
+        Author9 = (TextView)view.findViewById(R.id.return_fragment_author9);
+
+        Title1 = (TextView)view.findViewById(R.id.return_fragment_book_Title1);
+        Title2 = (TextView)view.findViewById(R.id.return_fragment_book_Title2);
+        Title3 = (TextView)view.findViewById(R.id.return_fragment_book_Title3);
+        Title4 = (TextView)view.findViewById(R.id.return_fragment_book_Title4);
+        Title5 = (TextView)view.findViewById(R.id.return_fragment_book_Title5);
+        Title6 = (TextView)view.findViewById(R.id.return_fragment_book_Title6);
+        Title7 = (TextView)view.findViewById(R.id.return_fragment_book_Title7);
+        Title8 = (TextView)view.findViewById(R.id.return_fragment_book_Title8);
+        Title9 = (TextView)view.findViewById(R.id.return_fragment_book_Title9);
+
+        Publisher1 = (TextView)view.findViewById(R.id.return_fragment_book_publisher1);
+        Publisher2 = (TextView)view.findViewById(R.id.return_fragment_book_publisher2);
+        Publisher3 = (TextView)view.findViewById(R.id.return_fragment_book_publisher3);
+        Publisher4 = (TextView)view.findViewById(R.id.return_fragment_book_publisher4);
+        Publisher5 = (TextView)view.findViewById(R.id.return_fragment_book_publisher5);
+        Publisher6 = (TextView)view.findViewById(R.id.return_fragment_book_publisher6);
+        Publisher7 = (TextView)view.findViewById(R.id.return_fragment_book_publisher7);
+        Publisher8 = (TextView)view.findViewById(R.id.return_fragment_book_publisher8);
+        Publisher9 = (TextView)view.findViewById(R.id.return_fragment_book_publisher9);
+
+        return_btn = (Button)view.findViewById(R.id.return_fragment_return_btn);
+        return_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return view;
     }
